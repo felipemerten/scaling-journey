05aeb7604f7127f8910204ea80920d93be79e004
felipemerten <felipemerten@gmail.com>
2016-11-10 20:43:03 -0200

    Teste do Hello, World na IDE: Eclipse

---
+++ monpoly/src/main/java/br/pucrs/fds/monpoly/App.java
@@ -0,0 +1,13 @@
+package br.pucrs.fds.monpoly;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
